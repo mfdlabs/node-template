7159388575a5f8ac610f6f772aba15dfe3248f94
Nikita Petko <petko@vmminfra.net>
2022-12-04 23:56:35 +0000

    Update index.ts
    Signed-off-by: Nikita Petko <petko@vmminfra.net>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*
-   Copyright 2022 MFDLABS Ops <ops@vmminfra.net>
+   Copyright 2022 JavaScript Squad <javascript-dev-squad@vmminfra.net>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
